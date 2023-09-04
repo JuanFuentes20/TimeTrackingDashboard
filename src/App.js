6d4dd986c04b5e82f20b95472cede18b04da87be
Juan Fuentes <59918928+JuanFuentes20@users.noreply.github.com>
2023-09-04 22:51:59 +0300

    Mobile version done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import activityData from './data.json'
 import MainWidget from './Components/MainWidget';
 import Activity from './Components/Activity';
 
-import { useState } from 'react';
-
-console.log(activityData)
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [period, setPeriod] = useState('daily');
+  const [period, setPeriod] = useState('weekly');
+
+  useEffect(() => {
+    if(period){
+      const timePeriodParagraphs = document.querySelectorAll('.time-period p');
+  
+    // Find the <p> element with the word "weekly" (case-insensitive)
+    timePeriodParagraphs.forEach(paragraph => {
+      if (paragraph.innerText.toLowerCase().includes(period)) {
+        paragraph.style.opacity = 1;
+        paragraph.style.fontWeight = 700 
+      }
+    })
+    }
+  }, [period]);
+  
 
   return (
     <div className='dashboard'>
@@ -16,6 +29,10 @@ function App() {
       {activityData.map(activity => {
         return (<Activity key={activity.title} period={period} activity={activity}/>)
       })}
+      <div className="footer attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+        Coded by <a href="https://www.frontendmentor.io/profile/JuanFuentes20">Juan Fuentes</a>.
+      </div>
     </div>
   );
 }
--- src/Components/MainWidget.js
+++ src/Components/MainWidget.js
@@ -1,14 +1,23 @@
 function MainWidget({setPeriod}) {
 
     const handlePeriod = (period) => {
-        setPeriod(prev => period) 
+        setPeriod(prev => {
+            
+            const timePeriodParagraphs = document.querySelectorAll('.time-period p')
+            timePeriodParagraphs.forEach(paragraph => {
+                if (paragraph.innerText.toLowerCase().includes(prev)) {
+                  paragraph.style.opacity = 0.7;
+                  paragraph.style.fontWeight = 100
+                }
+              })
+            return period}) 
     }
 
     return (
         <div className="main-widget">
             <div className="personal-section">
+                <img alt="profile icon" src="/images/image-jeremy.png"></img>
                 <div className="personal-info">
-                    <img alt="profile icon" src="/images/image-jeremy.png"></img>
                     <p>Report for</p>
                     <h1>Jeremy Robson</h1>
                 </div>
